da632e1a59b9dcd77a3027e6ec915572a2838c8f
akudenko <kydster95@gmail.com>
2022-10-25 21:06:59 +0300

    added another way to HW2

--- HW2/homeWork.py
+++ HW2/homeWork.py
@@ -5,3 +5,16 @@ i = 0
 while i < 5:
     i = i + 1
     print("#####")
+
+print(" ")
+
+print('#####\n' * 5)
+
+print(" ")
+
+print((('#' * 5) + '\n') * 5)
+
+print(" ")
+
+for i in range(5):
+    print('#####')
