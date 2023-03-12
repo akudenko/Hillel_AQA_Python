5b34d5b13b05213f11497a07c215de4e095628e1
Oleksii Kudenko <kydster95@gmail.com>
2023-03-12 22:01:42 +0200

    HW22 - removed abstract methods
    Signed-off-by: Oleksii Kudenko <kydster95@gmail.com>

--- HW22 - GOF/Bulider/builder_interface.py
+++ HW22 - GOF/Bulider/builder_interface.py
@@ -1,30 +1,25 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 
 
 class BuilderInterface(metaclass=ABCMeta):
     # Builder Interface
 
     @staticmethod
-    @abstractmethod
     def build_walls():
         pass
 
     @staticmethod
-    @abstractmethod
     def build_floor():
         pass
 
     @staticmethod
-    @abstractmethod
     def build_windows():
         pass
 
     @staticmethod
-    @abstractmethod
     def build_roof():
         pass
 
     @staticmethod
-    @abstractmethod
     def get_result():
         pass
